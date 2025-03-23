5681dd8cbf20f1fad3e4e65c2186efbf54192bea
Deepesh Barod <95953925+deepeshbarod80@users.noreply.github.com>
2025-03-24 02:40:16 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 // Define function
-def docker_build(String Project, String ImageTag, String DockerHubUser){
+def call(String Project, String ImageTag, String DockerHubUser){
   sh "docker build -t ${DockerHubUser}/${Project}:${ImageTag} ."
 }
