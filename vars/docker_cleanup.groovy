7d93fd3270aa64a70677df6d7937d04262f734f9
Deepesh Barod <95953925+deepeshbarod80@users.noreply.github.com>
2025-03-24 11:07:06 +0530

    Update docker_cleanup.groovy

--- vars/docker_cleanup.groovy
+++ vars/docker_cleanup.groovy
@@ -1,4 +1,24 @@
-// Removing Dockers Development
-def call(String Project, String ImageTag, String DockerHubUser){
-  sh "docker rmi ${DockerHubUser}/${Project}:${ImageTag}"
+def removeUnusedImages() {
+    echo "Removing unused Docker images..."
+    sh "docker image prune -af"
+}
+
+def removeDanglingImages() {
+    echo "Removing dangling Docker images..."
+    sh "docker images -f 'dangling=true' -q | xargs -r docker rmi -f"
+}
+
+def removeStoppedContainers() {
+    echo "Removing stopped Docker containers..."
+    sh "docker container prune -f"
+}
+
+def removeUnusedVolumes() {
+    echo "Removing unused Docker volumes..."
+    sh "docker volume prune -f"
+}
+
+def removeAllUnusedResources() {
+    echo "Removing all unused Docker resources (images, containers, volumes, networks)..."
+    sh "docker system prune -af"
 }
