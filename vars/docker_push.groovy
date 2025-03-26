b35bd802c95e3f3abab5cc8b3359c6dc0d9f0feb
Deepesh Barod <95953925+deepeshbarod80@users.noreply.github.com>
2025-03-26 21:32:07 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -4,7 +4,9 @@ withCredentials([usernamePassword(
   usernameVariable:"dockerHubUser", 
   passwordVariable:"dockerHubPass"
 )]){
-      echo ${env.dockerHubPass} | docker login -u ${env.dockerHubUser} --password-stdin
+      sh '''
+      echo "$dockerHubPass" | docker login -u "$dockerHubUser" --password-stdin
+      '''
       sh "docker image tag ${project}:${ImageTag} ${env.dockerHubUser}/${project}:${ImageTag}"
       sh "docker push ${env.dockerHubUser}/${project}:${ImageTag}"
     }
